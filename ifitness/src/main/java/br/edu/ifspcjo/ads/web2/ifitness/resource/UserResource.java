45728235ed458ea9b48406080d7b6e1e175b666c
CJ3032221 <CJ3032221@LAB01-PC04.ifspcjoad.ifspcjo.edu.br>
2026-04-14 20:30:49 -0300

    Persistencia de Dados e Requisição POST

--- ifitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/resource/UserResource.java
+++ ifitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/resource/UserResource.java
@@ -1,37 +1,46 @@
 package br.edu.ifspcjo.ads.web2.ifitness.resource;
 
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifspcjo.ads.web2.ifitness.domain.model.User;
+import br.edu.ifspcjo.ads.web2.ifitness.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 
 //Resource(ou Controller)
 
 @RestController // Annotation para quando chegar requisição spring progura classe para processar ela
+@RequestMapping("/users")
 public class UserResource {
 	
-	@GetMapping("/users") // @GetMapping = Mapear requisição GET (endpoint"/xpto")
-	public List<User> list(){
-		User user1 = new User();
-		user1.setId(1L);
-		user1.setName("Paulo Cezar");
-		user1.setEmail("paulocezzar@hotmail.com");
-		user1.setPassword("54321");
-		user1.setBirthDate(LocalDate.of(2004, 12, 18));
-		user1.setActive(true);
-		
-		User user2 = new User();
-		user2.setId(2L);
-		user2.setName("Paola Silva");
-		user2.setEmail("paolasisilva@hotmail.com");
-		user2.setPassword("12345");
-		user2.setBirthDate(LocalDate.of(2007, 05, 13));
-		user2.setActive(true);
-		
-		return Arrays.asList(user1, user2);
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping // @GetMapping = Mapear requisição GET (endpoint"/xpto") [Lê dados {Read}]
+	public List<User> read(){
+		return userRepository.findAll();
+	}
+	
+	@PostMapping // @PostMapping = mapear requisição POST (endpoint/"xpto") [Cria/muda dados {Create}]
+	public User create(@RequestBody User user, HttpServletResponse response) {
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent()) {
+			return ResponseEntity.ok(user.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
