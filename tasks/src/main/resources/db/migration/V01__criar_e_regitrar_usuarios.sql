42d5e045a10090f5441d5ed5fffee542649eda06
paulo <paulocezarleite91@gmail.com>
2026-05-02 20:15:40 -0300

    Solução do erro do mysql e parte inicial do exercicio concluida

--- tasks/src/main/resources/db/migration/V01__criar_e_regitrar_usuarios.sql
+++ tasks/src/main/resources/db/migration/V01__criar_e_regitrar_usuarios.sql
@@ -1,15 +1,15 @@
-CREATE TABLE user (
+CREATE TABLE users (
 	id bigint(20) NOT NULL AUTO_INCREMENT,
 	name varchar(200) NOT NULL,
 	password varchar(200) NOT NULL,
 	phone_number varchar(14) NOT NULL,
-	active tinyint(1) NOT NULL
+	active tinyint(1) NOT NULL,
 	PRIMARY KEY(id)
 ) 
 ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO user(id, name, password, phone_number, active)
+INSERT INTO users(id, name, password, phone_number, active)
 	values (1, 'Paulo Cezar', 'cjoweb2', '(12) 9999-999', 1);
 	
-INSERT INTO user(id, name, password, phone_number, active)
+INSERT INTO users(id, name, password, phone_number, active)
 	values (2, 'Paola Henrique', 'cjoweb333', '(12) 9999-888', 1);
